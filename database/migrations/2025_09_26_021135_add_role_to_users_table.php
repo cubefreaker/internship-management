aade4fc4481258021dcd6f032d9a9b7d18764551
cubefreaker <44396732+cubefreaker@users.noreply.github.com>
2025-10-04 11:12:06 +0700

    set default value

--- database/migrations/2025_09_26_021135_add_role_to_users_table.php
+++ database/migrations/2025_09_26_021135_add_role_to_users_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'siswa', 'guru', 'dudi'])->after('password');
+            $table->enum('role', ['admin', 'siswa', 'guru', 'dudi'])->after('password')->default('siswa');
         });
     }
 
